16ad31a587c2069578c1111af73eb923f529ecb2
KAndrade-SP <kaique.viana090@gmail.com>
2024-04-22 15:10:00 -0300

    Feedback de validação de dados

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,17 +35,24 @@ function subscribe() {
 
 function checkEmail(email) {
 
+    const spanElement = document.getElementById('subscribe-span')
+    const inputElement = document.getElementById('subscribe-input')
+
     const mail = email.trim()
     const validEmail = /^\S+@\S+\.\S+$/
 
     if (mail === '') {
-        console.log('campo vazio')
+        spanElement.innerText = "The email field is empty"
+        inputElement.className = "subscribe__input-invalid-data"
     } else {
 
         if (validEmail.test(mail)) {
+            spanElement.innerText = "Data sent successfully!"
+            inputElement.className = "subscribe__input-valid-data"
             return { email: mail }
         } else {
-            console.log('dados inválidos')
+            spanElement.innerText = "There is invalid data in the form"
+            inputElement.className = "subscribe__input-invalid-data"
         }
 
     }
--- pages/contact/assets/js/contact.js
+++ pages/contact/assets/js/contact.js
@@ -32,12 +32,12 @@ function sendMessage() {
         const userData = JSON.parse(localStorage.getItem('userData'))
         console.log(userData)
         // localStorage.removeItem('userData')
-    } else {
-        //false
     }
 }
 
 function checkData(firstName, lastName, email, message) {
+
+    const spanElement = document.getElementById('support-division-span')
 
     const fName = firstName.trim().toLowerCase()
     const lName = lastName.trim().toLowerCase()
@@ -48,18 +48,20 @@ function checkData(firstName, lastName, email, message) {
     const validEmail = /^\S+@\S+\.\S+$/
 
     if (fName === '' || lName === '' || mail === '' || msg === '') {
-        console.log('campos vazios')
+        spanElement.innerText = "There are empty fields in the form"
     } else {
-
         if (validName.test(fName) && validName.test(lName) && validEmail.test(mail)) {
+           
+            spanElement.innerText = "Data sent successfully!"
             const completeName = capitalizeName(fName, lName)
+
             return {
                 nome: completeName,
                 email: mail,
                 message: msg
             }
         } else {
-            console.log('dados inválidos')
+            spanElement.innerText = "There is invalid data in the form"
         }
 
     }
@@ -87,24 +89,29 @@ function subscribe() {
         const subscribeContactData = JSON.parse(localStorage.getItem('subscribeContactData'))
         console.log(subscribeContactData)
         // localStorage.removeItem('subscribeContactData')
-    } else {
-        //false
     }
 }
 
 function checkEmail(email) {
 
+    const spanElement = document.getElementById('subscribe-span')
+    const inputElement = document.getElementById('subscribe-input')
+
     const mail = email.trim()
     const validEmail = /^\S+@\S+\.\S+$/
 
     if (mail === '') {
-        console.log('campo vazio')
+        spanElement.innerText = "The email field is empty"
+        inputElement.className = "subscribe__input-invalid-data"
     } else {
 
         if (validEmail.test(mail)) {
+            spanElement.innerText = "Data sent successfully!"
+            inputElement.className = "subscribe__input-valid-data"
             return { email: mail }
         } else {
-            console.log('dados inválidos')
+            spanElement.innerText = "There is invalid data in the form"
+            inputElement.className = "subscribe__input-invalid-data"
         }
 
     }
